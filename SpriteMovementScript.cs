246b568618c71e1e176c9086b0a6cd370be4e3b5
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-01-05 22:53:33 +0100

    Create SpriteMovementScript.cs

---
+++ SpriteMovementScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+    int i = 0;
+    private SpriteRenderer spriteRenderer;
+    void Start(){
+
+    }
+
+    void Update(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Vector3 spritePosition = transform.position;
+
+        if (Input.GetKey("w") && spritePosition[1] < -2)
+        {
+            Debug.Log("Jump");
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+        }
+        if (Input.GetKey("a")){
+            Debug.Log("Left");
+            GetComponent<Rigidbody2D>().velocity = new Vector3(-2, 0, 0);
+        }
+        if (Input.GetKey("d")){
+            Debug.Log("Right");
+            GetComponent<Rigidbody2D>().velocity = new Vector3(2, 0, 0);
+        }
+        if (Input.GetKey("f")){
+            Debug.Log("Dash");
+            GetComponent<Rigidbody2D>().velocity = new Vector3(10, 0, 0);
+        }
+
+    }
+}
