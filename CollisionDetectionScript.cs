95683c63fd5e399ad95d96035a4378aa2d1be53e
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-01-09 23:13:45 +0100

    Update CollisionDetectionScript.cs

--- CollisionDetectionScript.cs
+++ CollisionDetectionScript.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
 
-public class TerrainCollisionDetection : MonoBehaviour
-{
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Terrain")){
-            // Action if Sprite hit an Object
+public class TerrainCollisionDetection : MonoBehaviour{
+    
+    [SerializeField] private string terrainTag = "Terrain";
+    
+    private void OnCollisionEnter(Collision collision){
+        CheckTerrainCollision(collision);
+    }
+    
+    private void CheckTerrainCollision(Collision collision){
+        if (collision != null && collision.gameObject.CompareTag(terrainTag)){
+            HandleTerrainCollision(collision.gameObject);
         }
+    }
+    
+    private void HandleTerrainCollision(GameObject terrainObject){
+        Debug.Log("Collided with terrain: " + terrainObject.name);
+        gameObject.SetActive(false);
     }
 }
