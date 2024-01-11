ff2e7ec068e4a724af6dc1b80735b58955af2ba5
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-01-11 13:45:31 +0100

    Update SpriteMovementScript.cs

--- SpriteMovementScript.cs
+++ SpriteMovementScript.cs
@@ -5,22 +5,41 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour{
     int i = 0;
     private Rigidbody2D rigidbody;
+    private Animator animation;
+    private float HorizontalMovement = 0f;
+    private SpriteRenderer sprite;
 
     private void Start(){
         rigidbody = GetComponent<Rigidbody2D>();
+        animation = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     private void Update(){
-        float HorizontalMovement = Input.GetAxisRaw("Horizontal");
+        HorizontalMovement = Input.GetAxisRaw("Horizontal");
         rigidbody.velocity = new Vector2(7f * HorizontalMovement, rigidbody.velocity.y);
 
-        if (Input.GetButton("Jump")){
+        if (Input.GetButtonDown("Jump")){
            rigidbody.velocity = new Vector2(rigidbody.velocity.x , 14f);
         }
 
-        if (Input.GetKeyDown("f")){
-            rigidbody.velocity = new Vector2(14f * HorizontalMovement, rigidbody.velocity.y);
+        if (rigidbody.position.y < -20) {
+            rigidbody.position = new Vector2(-16.25f, -6.5f);
+            Debug.Log(rigidbody.velocity.y);
         }
 
+        updateAnimations();
+    }
+
+    void updateAnimations(){
+        if (HorizontalMovement > 0f){
+            animation.SetBool("isRunning", true);
+            sprite.flipX = false;
+        }else if (HorizontalMovement < 0f){
+            animation.SetBool("isRunning", true);
+            sprite.flipX = true;
+        }else{
+            animation.SetBool("isRunning", false);
+        }
     }
 }
