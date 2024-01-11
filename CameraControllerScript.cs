63c93f9a933a663c9584080489e4a5828b3cfa11
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-01-11 23:21:07 +0100

    Update CameraControllerScript.cs

--- CameraControllerScript.cs
+++ CameraControllerScript.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class CameraController : MonoBehaviour{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField] private Transform player;
+    
+
+    private void Update(){
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 }
