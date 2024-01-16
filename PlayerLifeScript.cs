333d616bfbab8c8b4231581e07297be5f544de5d
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-01-16 10:03:21 +0100

    Update PlayerLifeScript.cs

--- PlayerLifeScript.cs
+++ PlayerLifeScript.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour{
 
+    private Rigidbody2D rigidbody;
     private Animator animator;
 
     private void Start(){
         animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision){
@@ -17,6 +20,11 @@ public class PlayerLife : MonoBehaviour{
     }
 
     private void Death(){
+        rigidbody.bodyType = RigidbodyType2D.Static;
         animator.SetTrigger("death");
+    }
+
+    private void RestartLevel(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
