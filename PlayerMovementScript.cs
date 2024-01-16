f2830301f61c50e122d4e13ee3015a131675bb1e
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-01-16 10:01:12 +0100

    Update PlayerMovementScript.cs

--- PlayerMovementScript.cs
+++ PlayerMovementScript.cs
@@ -1,65 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerMovement : MonoBehaviour{
-    [SerializeField] private LayerMask JumpableGround;
-    private BoxCollider2D collider2D;
+public class PlayerLife : MonoBehaviour{
+
     private Rigidbody2D rigidbody;
-    private Animator animation;
-    private float HorizontalMovement = 0f;
-    private SpriteRenderer sprite;
-    private Vector2 spawnPosition = new Vector2(-16.25f, -6.5f);
-    [SerializeField] private float JumpForce = 14f;
-    [SerializeField] private float MoveSpeed = 7f;
-    private enum MovementState { idle, running, falling, jumping }
-    private MovementState state = MovementState.idle;
-    
+    private Animator animator;
+
     private void Start(){
+        animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody2D>();
-        animation = GetComponent<Animator>();
-        collider2D = GetComponent<BoxCollider2D>();
-        sprite = GetComponent<SpriteRenderer>();
     }
 
-    private void Update() {
-        HorizontalMovement = Input.GetAxisRaw("Horizontal");
-        rigidbody.velocity = new Vector2(MoveSpeed * HorizontalMovement, rigidbody.velocity.y);
-
-        if (Input.GetButton("Jump") && isGrounded()) {
-            rigidbody.velocity = new Vector2(rigidbody.velocity.x, JumpForce);
-        }
-        if (Input.GetKey("f")){
-            rigidbody.position = new Vector2(rigidbody.position.x + HorizontalMovement/10, rigidbody.position.y);
-        }
-        if (rigidbody.position.y < -20) {
-            rigidbody.position = spawnPosition;
-            Debug.Log(rigidbody.velocity.y);
-        }
-        updateAnimations();
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Trap")){
+            Death();
+        }    
     }
 
-    void updateAnimations(){
-
-        MovementState state;
-        if (HorizontalMovement > 0f){
-            state = MovementState.running;
-            sprite.flipX = false;
-        }else if (HorizontalMovement < 0f){
-            state = MovementState.running;
-            sprite.flipX = true;
-        }else{
-            state = MovementState.idle;
-        }
-        if(rigidbody.velocity.y > .1f){
-            state = MovementState.jumping;
-        }else if(rigidbody.velocity.y < -.1f){
-            state = MovementState.falling;
-        }
-        animation.SetInteger("state", (int)state);
+    private void Death(){
+        rigidbody.bodyType = RigidbodyType2D.Static;
+        animator.SetTrigger("death");
     }
 
-    private bool isGrounded() {
-        return Physics2D.BoxCast(collider2D.bounds.center, collider2D.bounds.size, 0f, Vector2.down, .1f,  JumpableGround);
+    private void RestartLevel(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
