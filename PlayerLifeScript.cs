1e01db271412a4cab09f3f9e16fd481411864373
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-01-16 09:36:02 +0100

    Update PlayerLifeScript.cs

--- PlayerLifeScript.cs
+++ PlayerLifeScript.cs
@@ -3,7 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerLife : MonoBehaviour{
+
+    private Animator animator;
+
     private void Start(){
-        
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Trap")){
+            Death();
+        }    
+    }
+
+    private void Death(){
+        animator.SetTrigger("death");
     }
 }
