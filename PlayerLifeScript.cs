691c9a5db19fd84a90981a86637527d6dc7dcb9c
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-01-18 22:31:02 +0100

    Update PlayerLifeScript.cs

--- PlayerLifeScript.cs
+++ PlayerLifeScript.cs
@@ -4,26 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour{
-
     private Rigidbody2D rigidbody;
     private Animator animator;
-
     private void Start(){
         animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody2D>();
     }
-
     private void OnCollisionEnter2D(Collision2D collision){
         if (collision.gameObject.CompareTag("Trap")){
             Death();
         }    
     }
-
     private void Death(){
         rigidbody.bodyType = RigidbodyType2D.Static;
         animator.SetTrigger("death");
     }
-
     private void RestartLevel(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
