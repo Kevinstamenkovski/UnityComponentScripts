34add3293cc774071cc742ddee5b72dfaca6c4a3
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-01-18 22:31:25 +0100

    Update CollisionDetectionScript.cs

--- CollisionDetectionScript.cs
+++ CollisionDetectionScript.cs
@@ -3,19 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class TerrainCollisionDetection : MonoBehaviour{
-    
     [SerializeField] private string terrainTag = "Terrain";
-    
     private void OnCollisionEnter(Collision collision){
         CheckTerrainCollision(collision);
     }
-    
     private void CheckTerrainCollision(Collision collision){
         if (collision != null && collision.gameObject.CompareTag(terrainTag)){
             HandleTerrainCollision(collision.gameObject);
         }
     }
-    
     private void HandleTerrainCollision(GameObject terrainObject){
         Debug.Log("Collided with terrain: " + terrainObject.name);
         gameObject.SetActive(false);
