b172d82fcb23df2a51b279a9ab964404e050a1af
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-01-21 21:27:21 +0100

    Update PlayerMovementScript.cs

--- PlayerMovementScript.cs
+++ PlayerMovementScript.cs
@@ -34,7 +34,6 @@ public class PlayerMovement : MonoBehaviour{
         }
         updateAnimations();
     }
-
     void updateAnimations(){
         MovementState state;
         if (HorizontalMovement > 0f){
