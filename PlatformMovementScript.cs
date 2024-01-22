924f4ebc5225e4f996adf1c35e1a386685b4ae80
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-01-22 23:45:15 +0100

    Update PlatformMovementScript.cs

--- PlatformMovementScript.cs
+++ PlatformMovementScript.cs
@@ -1,1 +1,21 @@
+using UnityEngine;
 
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed = 5f;
+    public float distance = 5f;
+
+    private Vector3 initialPosition;
+    private float direction = 1;  
+
+    void Start(){
+        initialPosition = transform.position;
+    }
+
+    void Update(){
+        float newPosition = Mathf.PingPong(Time.time * speed, distance);
+        Vector3 targetPosition = initialPosition + Vector3.right * newPosition * direction;
+
+        transform.position = targetPosition;
+    }
+}
