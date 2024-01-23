4a7f64495b67cd000c360c092160db707675235c
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-01-23 22:55:03 +0100

    Update FallDeathScript.cs

--- FallDeathScript.cs
+++ FallDeathScript.cs
@@ -5,5 +5,16 @@ public class FallDeathScript : MonoBehaviour{
   private BoxCollider2D collider2D;
   private Rigidbody2D rigidbody;
   [SerializeField] private int fallLimit = -20;
-  
+  private Vector2 spawnPosition = new Vector2(-16.25f, -6.5f);
+
+  private void Start(){
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider2D = GetComponent<BoxCollider2D>();
+    }
+  private void Update(){
+    if (rigidbody.position.y < fallLimit) {
+      rigidbody.position = spawnPosition;
+      Debug.Log(rigidbody.velocity.y);
+    }
+  }
 }
