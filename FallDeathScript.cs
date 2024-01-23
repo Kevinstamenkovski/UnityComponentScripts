15169459a70036830b0ab696a578c7512b9bf05c
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-01-23 22:53:00 +0100

    Update FallDeathScript.cs

--- FallDeathScript.cs
+++ FallDeathScript.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class FallDeathScript : MonoBehaviour{
+  private BoxCollider2D collider2D;
+  private Rigidbody2D rigidbody;
   [SerializeField] private int fallLimit = -20;
   
 }
