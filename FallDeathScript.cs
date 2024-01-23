ddb988108f1e4edf75ec4ff5f6e0cd36e7fe05be
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-01-23 22:52:33 +0100

    Update FallDeathScript.cs

--- FallDeathScript.cs
+++ FallDeathScript.cs
@@ -2,5 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class FallDeathScript : MonoBehaviour{
+  [SerializeField] private int fallLimit = -20;
   
 }
