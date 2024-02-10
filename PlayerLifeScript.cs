f38d5c84eed2c5c11aa1aa84befb06f8588f32c3
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-02-10 22:18:04 +0100

    Update PlayerLifeScript.cs

--- PlayerLifeScript.cs
+++ PlayerLifeScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class PlayerLife : MonoBehaviour{
     private Rigidbody2D rigidbody;
     private Animator animator;
