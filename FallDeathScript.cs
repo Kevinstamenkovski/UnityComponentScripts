0fe15e9aae71c94cf42565f11ef9974f17eb24a4
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-02-21 22:39:01 +0100

    Update FallDeathScript.cs

--- FallDeathScript.cs
+++ FallDeathScript.cs
@@ -8,7 +8,6 @@ public class FallDeathScript : MonoBehaviour{
   private Vector2 spawnPosition = new Vector2(-16.25f, -6.5f);
   private void Start(){
         rigidbody = GetComponent<Rigidbody2D>();
-  
         collider2D = GetComponent<BoxCollider2D>();
     }
   private void Update(){
