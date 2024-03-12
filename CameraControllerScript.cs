a933cde32bac4442319a08a1d484ad67c03942ed
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-03-12 22:19:03 +0100

    Update CameraControllerScript.cs

--- CameraControllerScript.cs
+++ CameraControllerScript.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class CameraController : MonoBehaviour{
     [SerializeField] private Transform player;
     private void Update(){
