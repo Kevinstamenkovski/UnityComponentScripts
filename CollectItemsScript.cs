80234b5dec9c9c2b924eac9e858edf0cf0a34104
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-05-08 23:57:36 +0200

    Update CollectItemsScript.cs

--- CollectItemsScript.cs
+++ CollectItemsScript.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI; 
 public class ItemCollector : MonoBehaviour{
     private int cherries = 0; 
     [SerializeField] private Text cherriesText;
