2d907c180fb9d6b8cd1101d408a8ffd0a7af72eb
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-05-08 23:57:32 +0200

    Update CollectItemsScript.cs

--- CollectItemsScript.cs
+++ CollectItemsScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class ItemCollector : MonoBehaviour{
-    private int cherries = 0;
+    private int cherries = 0; 
     [SerializeField] private Text cherriesText;
     private void OnTriggerEnter2D(Collider2D collision){
         if (collision.gameObject.CompareTag("Cherry")){
